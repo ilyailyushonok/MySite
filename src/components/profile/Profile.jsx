67337f32bfdbc300cd608601ef8254867eac07da
ilyailyushonok <ilyushonok2014@mail.ru>
2024-12-28 13:32:04 +0300

    beginning FLUX _33les

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MyPosts from "./MyPosts/MyPosts";
-import s from './Profile.module.css'
+import s from "./Profile.module.css"
 import ProfileInfo from "./profileInfo/ProfileInfo";
 
 const Profile = (props) => {
